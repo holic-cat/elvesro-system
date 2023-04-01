daf49214ada83c07b8212416e1d6f160115d7cb1
raffles <120664320+developer20221215@users.noreply.github.com>
2023-04-01 17:52:48 +0800

    Update elves_pre.lua

--- elves_pre.lua
+++ elves_pre.lua
@@ -81887,7 +81887,36 @@ tbl = {
 		},
 		slotCount = 0,
 		ClassNum = 13,
-	},	
+	},
+	[400131] = {
+		unidentifiedDisplayName = "Unidentified Helmet",
+		unidentifiedResourceName = "Çï¸§",
+		unidentifiedDescriptionName = { "Can be identified by using a ^990099Magnifier^000000." },
+		identifiedDisplayName = "Diadem of Brynhild Kaho's Horn",
+		identifiedResourceName = "ºê·ôÈúÆ®ÀÇ¿Õ°ü",
+		identifiedDescriptionName = {
+			"This diadem has been worn by a heroic woman. The legend says she had supernatural powers.",
+			"You can feel a strong magic flow when wearing it.",
+			"_______________________",
+			"STR +5",
+			"INT +5",
+			"VIT +10",
+			"AGI +10",
+			"LUK +20",
+			"MDEF +10",
+			"_______________________",
+			"^0000CCType:^000000 Headgear",
+			"^0000CCDefense:^000000 5",
+			"^0000CCPosition:^000000 Upper",
+			"^0000CCWeight:^000000 10",
+			"^0000CCArmor Level:^000000 1",
+			"_______________________",
+			"^0000CCRequirement:^000000 None"
+		},
+		slotCount = 4,
+		ClassNum = 1057,
+		costume = false
+	},
 }
 
 -- Now for a helper function because i hate repetitions 
