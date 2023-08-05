9ea1a75ac8293e2ab00f361cc7b168001d6e3d18
raffles <53240858+holic-cat@users.noreply.github.com>
2023-08-06 00:59:41 +0800

    1

--- elves_pre.lua
+++ elves_pre.lua
@@ -86464,7 +86464,35 @@ tbl = {
 		ClassNum = 479,
 		costume = true
 	},
-
+	[20294] = {
+		unidentifiedDisplayName = "Unidentified Hat",
+		unidentifiedResourceName = "캡",
+		unidentifiedDescriptionName = { "Can be identified by using a ^990099Magnifier^000000." },
+		identifiedDisplayName = "Savage Babe Kaho's Horn",
+		identifiedResourceName = "세비지베베모자",
+		identifiedDescriptionName = {
+			"The Savage Babe of the Savage family. A costume hat made in honor of the 29th president.",
+			"He has the courage to move toward a huge enemy even though he is small.",
+			"_______________________",
+			"STR +5",
+			"INT +5",
+			"VIT +10",
+			"AGI +10",
+			"LUK +20",
+			"MDEF +10",
+			"_______________________",
+			"^0000CCType:^000000 Headgear",
+			"^0000CCDefense:^000000 5",
+			"^0000CCPosition:^000000 Upper",
+			"^0000CCWeight:^000000 10",
+			"^0000CCArmor Level:^000000 1",
+			"_______________________",
+			"^0000CCRequirement:^000000 None"
+		},
+		slotCount = 4,
+		ClassNum = 553,
+		costume = true
+	},
 }
 
 -- Now for a helper function because i hate repetitions 
